eb4ff8fc99f679ea89485e7a9156d1988827361a
S-e-cmd <ewigkeitein@gmail.com>
2026-08-09 17:21:35 +0900

    cleanup: remove unused staged rotation utility

--- client/rotation-utils.js
+++
@@ -1,5 +0,0 @@
-export function isRotationPlan(x){return x?.type==='plan'&&x?.rotationName&&Number(x?.rotationOrder)>0}
-
-export function activeRotationPlans(plans){return(plans||[]).filter(x=>isRotationPlan(x)&&x.status!=='完了'&&x.status!=='中止')}
-
-export function rotationActuals(actuals,name){return(actuals||[]).filter(x=>x.rotationName===name).sort((a,b)=>(a.date||'').localeCompare(b.date||''))}
